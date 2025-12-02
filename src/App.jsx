b6393234d6e5333a944c86fa2f151bf8a500c0e2
Marco-Rocco <marcorocco28@icloud.com>
2025-12-02 23:09:04 +0100

    feat: implement api fetch with axios and print response data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 
 function App() {
-  let [city, setCity] = useState("")
+  let [city, setCity] = useState("");
+  let [weatherData, setWeatherData] = useState(null);
+  const weatherKey = import.meta.env.VITE_API_KEY;
+
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}`
+
+  function getWeather() {
+    axios.get(URL)
+      .then((res) => {
+        setWeatherData(res.data)
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
 
   return (
     <>
       <input type="text" placeholder='Search city' value={city} onChange={(e) => setCity(e.target.value)} />
+      <button onClick={getWeather}>Cerca</button>
       <p>{city}</p>
     </>
   )
+
 }
 
 export default App
